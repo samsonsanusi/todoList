5bf7649822ccc3c09e8f99b65253a696f4acb921
samsonsanusi <samsonsanusi96@gmail.com>
2023-06-19 17:28:26 +0100

    build/added functionality

---
+++ index.js
@@ -0,0 +1,82 @@
+//Target html element
+const userInput = document.getElementById('userInput');
+const addTodo = document.getElementById('addTodo');
+const ul = document.querySelector("ul")
+const item = document.querySelector('li')
+const response = document.querySelector(".response")
+const mainContainer = document.getElementById('mainContainer')
+const firstIntro = document.querySelector('.firstIntro');
+const secondIntro = document.querySelector('secondIntro')
+const sam = document.getElementById("sam");
+
+
+//style
+// document.body.style.backgroundColor = '#04A1BF';
+document.body.style.fontFamily = "sans-serif";
+document.body.style.display = "flex";
+document.body.style.flexDirection = "column";
+document.body.style.justifyContent = "center";
+document.body.style.alignContent = "center"
+document.body.style.height = "100vh"
+// firstIntro.style.color = '#FFCD5D'
+
+//check for the length of the value entered
+const inputLength = () =>{
+  return userInput.value.length;
+}
+
+//checks for the list of items
+const listLength = () =>{
+  return item.length;
+}
+
+//create li in ul element 
+const createLisElement = () =>{
+  // preventDefault(e)
+  let li = document.createElement('li')
+  li.appendChild(document.createTextNode(userInput.value))
+  ul.appendChild(li)
+  userInput.value = ''
+  // strike through
+  //it adds the done style once because it's in this function
+
+  const crossOut = () => {
+    li.classList.toggle('done')
+  }
+  li.addEventListener('click', crossOut)
+
+  // Start add Delete Button
+  let dBtn = document.createElement('button')
+  dBtn.appendChild(document.createTextNode('X'))
+  li.appendChild(dBtn)
+  const deleteItem = () => {
+    li.classList.add('delete')
+  }
+  dBtn.addEventListener('click', deleteItem)
+  // End add Delete Button
+
+  //Add class Delete (Display: none)
+  
+  //End Add class Delete
+}
+
+const addListAfterClick = () =>{
+  if (inputLength() > 0) {
+     createLisElement();
+  }else if (inputLength() === "") {
+    alert("Enter valid Item")
+  }
+}
+//this checks if you click the Enter button
+const addListAfterKeyPress = (e) =>{
+  if(inputLength() > 0 && e.which === 13){
+    createLisElement();
+  }
+}
+
+addTodo.addEventListener('click', addListAfterClick);
+
+userInput.addEventListener('keypress', addListAfterKeyPress);
+
+
+
